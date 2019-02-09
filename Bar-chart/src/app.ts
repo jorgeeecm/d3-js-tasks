6070b3934e1f7060c41c2112b192e9d4d4948efa
Jorge <jorgecastillamaldonado@gmail.com>
2019-02-09 13:56:25 +0100

    Bar chart created

--- Bar-chart/src/app.ts
+++ Bar-chart/src/app.ts
@@ -2,3 +2,4 @@ import "./app.scss";
 
 // Import here your D3 visualizations.
 import "./d3/title.d3";
+import "./d3/barchart.d3"
---
+++ Bar-chart/src/d3/barchart.d3.ts
@@ -0,0 +1,83 @@
+import { select } from "d3-selection";
+import { scaleLinear,  scaleOrdinal, scaleBand } from "d3-scale";
+import { interpolatePlasma } from "d3-scale-chromatic";
+import { avgTemp } from "./barchart.data";
+import { axisBottom, axisLeft } from "d3-axis";
+import { extent } from "d3-array";
+
+const d3 = {
+  select,
+  scaleLinear,
+  scaleBand,
+  extent,
+  axisBottom,
+  axisLeft,
+  scaleOrdinal,
+  interpolatePlasma,
+ 
+};
+
+const width = 500;
+const height = 300;
+const padding = 50;
+
+//Definiing the months
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+//Create the new card
+const card = d3
+  .select("#root")
+  .append("div")
+  .attr("class", "card");
+
+//Create the svg canvas
+const svg = card
+  .append("svg")
+  .attr("width", "100%")
+  .attr("height", "100%")
+  .attr("viewBox", `${-padding} ${-padding} ${width + 2 * padding} ${height + 2 * padding}`);
+
+//Scaling the temperatures (y axis)
+const yScalePos = d3.scaleLinear()
+  .domain(d3.extent(avgTemp)) //Catching the max and min value for the temps
+  .range([height, 0]);
+
+//Scaling the months (x axis)
+const xScalePos = d3.scaleBand<string>()
+  .domain(months) // Jan to Dec 2018
+  .range([0, width])
+  .paddingInner(0.05);
+
+//Let's define a scale to assing color depending on the temperature values
+const temperatureLogScale = scaleLinear()
+  .domain(d3.extent(avgTemp))
+  .range([0, 1]);
+
+const colorScale = (temp: number) => d3.interpolatePlasma(temperatureLogScale(temp));
+
+//Lets draw the bar, making a group will give us the support for futures updates
+const barGroup = svg
+  .append("g")
+
+barGroup
+  .selectAll('rect')
+  .data(avgTemp)
+  .enter()
+  .append("rect")
+    .attr("x", ((d,i) => xScalePos(months[i])))
+    .attr("y", d => yScalePos(d))
+    .attr("width", xScalePos.bandwidth())
+    .attr("height", d => height - yScalePos(d))
+    .attr("fill", d => colorScale(d));
+
+
+const axisGroup = svg.append("g");
+
+// Y Axis: call axisLeft helper and pass the scale
+axisGroup.append("g").call(d3.axisLeft(yScalePos));
+
+// X axis:
+axisGroup
+  .append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(d3.axisBottom(xScalePos));
